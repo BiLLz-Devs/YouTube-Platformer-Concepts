b91545ffb55c723ebe6d5f7a1f6875cccf03485e
BiLLz <75352995+BiLLz-BiZiLLiAMz@users.noreply.github.com>
2024-11-11 09:22:41 -0600

    Updates
    - planned next few video ideas

---
+++ 05 - Wall Grab and Climb/Entity/Player/State Machine/States.gd
@@ -0,0 +1,22 @@
+extends Node
+
+@onready var Locked = $Locked
+@onready var Idle = $Idle
+@onready var Run = $Run
+@onready var Jump = $Jump
+@onready var JumpPeak = $JumpPeak
+@onready var WallJump = $WallJump
+@onready var Fall = $Fall
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _draw():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
