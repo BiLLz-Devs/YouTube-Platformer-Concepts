07b5de85e3ca11672b0395b0d9d8fe446a28333a
BiLLz <75352995+BiLLz-BiZiLLiAMz@users.noreply.github.com>
2024-11-11 09:49:45 -0600

    Updates

---
+++ 05 - Wall Grab and Climb/Entity/Player/State Machine/WallClimbState.gd
@@ -0,0 +1,17 @@
+extends PlayerState
+
+
+func EnterState():
+	Name = "WallClimb"
+
+
+func ExitState():
+	pass
+
+
+func Draw():
+	pass
+
+
+func Update(delta):
+	pass
---
+++ 05 - Wall Grab and Climb/Entity/Player/State Machine/WallGrabState.gd
@@ -0,0 +1,17 @@
+extends PlayerState
+
+
+func EnterState():
+	Name = "WallGrab"
+
+
+func ExitState():
+	pass
+
+
+func Draw():
+	pass
+
+
+func Update(delta):
+	pass
