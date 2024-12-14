fb7c26f73885dc04a2222b6b920bb91118c6c9ed
BiLLz <75352995+BiLLz-BiZiLLiAMz@users.noreply.github.com>
2024-12-14 10:22:27 -0600

    Update
    _ reupload of specialty platforms

---
+++ 09 - Other Specialty Platforms/Entity/Player/State Machine/LedgeGrabState.gd
@@ -0,0 +1,91 @@
+extends PlayerState
+
+const ledgeReleaseXNudge = 1
+const ledgeReleaseYNudge = 5
+
+var cornerGrabPosition = Vector2.ZERO
+var ledgeGrabSnapPosition = Vector2.ZERO
+
+func EnterState():
+	# Set the label
+	Name = "LedgeGrab"
+	if (Player.RCLedgeLeftLower.is_colliding()):
+		Player.ledgeDirection = Vector2.LEFT
+	elif (Player.RCLedgeRightLower.is_colliding()):
+		Player.ledgeDirection = Vector2.RIGHT
+	
+	# Snap the player to the corner of the ledge
+	# Get the tile data to find the top left or top right corner global_position
+	var _tileSize = Player.CollisionMap.tile_set.tile_size # Returns a Vector2 of the tile size
+	var _tileSizeCorrection = (_tileSize / 2) as Vector2 # Adjusts for center position of tile to give us the corner of the tile
+	var _collisionPoint # Where the raycast is colliding
+	var _tileCoords # The coordinates of the colliding tile
+	
+	if (Player.ledgeDirection == Vector2.LEFT):
+		if (Player.RCLedgeLeftLower.is_colliding()):
+			_collisionPoint = Player.RCLedgeLeftLower.get_collision_point() # Get the colliding point
+			_tileCoords = Player.CollisionMap.local_to_map(_collisionPoint) # Convert to tilemap coordinates
+			
+			# Check if this is a one way collision
+			# The following code gets the data of the ledge tile
+			var _ledge = Vector2i(_tileCoords.x + Player.facing, _tileCoords.y)
+			var _tileData = Player.CollisionMap.get_cell_tile_data(_ledge)
+			if (_tileData.get_custom_data("OneWay")):
+				print("One Way Left")
+				return
+			else:
+				cornerGrabPosition = Player.CollisionMap.map_to_local(_tileCoords) - _tileSizeCorrection
+	elif (Player.ledgeDirection == Vector2.RIGHT):
+		if (Player.RCLedgeRightLower.is_colliding()):
+			_collisionPoint = Player.RCLedgeRightLower.get_collision_point() # Get the colliding point
+			_tileCoords = Player.CollisionMap.local_to_map(_collisionPoint) # Convert to tilemap coordinates
+			
+			# Check if this is a one way collision
+			# The following code gets the data of the ledge tile
+			var _ledge = Vector2i(_tileCoords.x + Player.facing, _tileCoords.y)
+			var _tileData = Player.CollisionMap.get_cell_tile_data(_ledge)
+			if(_tileData.get_custom_data("OneWay")):
+				print("One Way Right")
+				return
+			else:
+				cornerGrabPosition = Player.CollisionMap.map_to_local(_tileCoords) - _tileSizeCorrection
+	
+	ledgeGrabSnapPosition = Vector2(cornerGrabPosition.x + (Player.ledgeDirection.x * -1), cornerGrabPosition.y + 6)
+	Player.global_position = ledgeGrabSnapPosition
+
+
+func ExitState():
+	pass
+
+
+func Update(delta: float):
+	Player.climbStamina -= Player.GrabStaminaCost
+	HandleJumpUp()
+	HandleClimbUp()
+	HandleLedgeRelease()
+	HandleAnimations()
+
+
+func HandleLedgeRelease():
+	if (Player.keyDown or (Player.climbStamina <= 0)):
+		Player.global_position += Vector2(ledgeReleaseXNudge * Player.ledgeDirection.x * -1,
+			ledgeReleaseYNudge)
+		Player.ChangeState(States.Fall)
+
+
+# Functionality for if the player jumps from a grab
+func HandleJumpUp():
+	if (Player.keyJumpPressed):
+		Player.global_position += Vector2(ledgeReleaseXNudge * Player.ledgeDirection.x * -2, ledgeReleaseYNudge)
+		Player.ChangeState(States.Jump)
+
+
+# Functionality for if the player climbs from a grab
+func HandleClimbUp():
+	if (Player.keyUpPressed):
+		Player.ChangeState(States.LedgeClimb)
+
+
+func HandleAnimations():
+	Player.Animator.play("LedgeGrab")
+	Player.Sprite.flip_h = (Player.ledgeDirection.x < 0)
---
+++ 09 - Other Specialty Platforms/Entity/Player/State Machine/WallClimbState.gd
@@ -0,0 +1,45 @@
+extends PlayerState
+
+
+func EnterState():
+	Name = "WallClimb"
+
+
+func ExitState():
+	pass
+
+
+func Draw():
+	pass
+
+
+func Update(delta):
+	Player.GetCanWallClimb()
+	Player.climbStamina -= Player.ClimbStaminaCost
+	HandleClimbMovement()
+	Player.HandleWallRelease()
+	Player.HandleWallJump()
+	Player.HandleDash()
+	HandleAnimations()
+
+
+func HandleClimbMovement():
+	if (Player.keyClimb):
+		if (Player.wallClimbDirection != Vector2.ZERO):
+			if (Player.keyUp and (Player.RCUpperLeft.is_colliding() or Player.RCUpperRight.is_colliding())):
+				Player.velocity.y = -Player.ClimbSpeed
+			elif (Player.keyDown and (Player.RCLowerLeft.is_colliding() or Player.RCLowerRight.is_colliding())):
+				Player.velocity.y = Player.ClimbSpeed
+			else:
+				Player.ChangeState(States.WallGrab)
+	else:
+		Player.ChangeState(States.Fall)
+
+
+func HandleAnimations():
+	if (Player.velocity.y > 0):
+		Player.Animator.speed_scale = -1
+	else:
+		Player.Animator.speed_scale = 1
+	Player.Animator.play("WallClimb")
+	Player.Sprite.flip_h = (Player.wallClimbDirection == Vector2.LEFT)
